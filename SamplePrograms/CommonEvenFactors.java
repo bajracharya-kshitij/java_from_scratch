00f97a0af49e4d430e1624bc03b5b65da40a0b73
Kshitij Bajracharya <bajracharya.kshitij@gmail.com>
2018-09-17 16:28:29 +0545

    Fix indentation

--- SamplePrograms/CommonEvenFactors.java
+++ SamplePrograms/CommonEvenFactors.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 class CommonEvenFactors {
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     System.out.println("Enter first number: ");
     int n1 = scanner.nextInt();
